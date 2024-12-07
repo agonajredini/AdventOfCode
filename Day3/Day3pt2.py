bfc24cb8900710ea73fab594dba76fa2594c00f1
Agon Ajredini <agonajrediniyt@gmail.com>
2024-12-07 23:05:10 +0100

    Completed Day 3 pt2

---
+++ Day3/Day3pt2.py
@@ -0,0 +1,25 @@
+import re
+memory = ""
+with open("C:/Users/Life's Good/Desktop/AdventCode/Day3/input.txt") as f:
+    memory = f.read()
+
+def toggle(text):
+    is_enabled = True
+    results = []
+    
+    pattern = r'(mul\((\d+),(\d+)\)|do\(\)|don\'t\(\))'
+    matches = re.findall(pattern, text)
+    
+    for match in matches:
+        if match[0].startswith('mul'):
+            if is_enabled:
+                results.append(int(match[1])*int(match[2]))
+        
+        elif match[0] == 'do()':
+            is_enabled = True
+        elif match[0] == 'don\'t()':
+            is_enabled = False
+    return sum(results)
+
+results = toggle(memory)
+print(results)
