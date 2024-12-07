4cdbe7af24e11743fc3aad00a8ea0cd728d47630
Agon Ajredini <agonajrediniyt@gmail.com>
2024-12-07 21:57:13 +0100

    Completed Day2 pt1

---
+++ Day2/Day2.py
@@ -0,0 +1,22 @@
+lists = []
+def check_asc_or_desc(lst):
+    results = []
+    for sublist in lst:
+        is_ordered = all(sublist[i] < sublist[i + 1] for i in range(len(sublist) - 1))or \
+           all(sublist[i] >= sublist[i+1] for i in range(len(sublist) - 1))
+           
+        check_diff = any(abs(sublist[i] - sublist[i+1]) > 3 or abs(sublist[i] - sublist[i+1]) < 1   for i in range(len(sublist) - 1))
+        
+        if is_ordered and not check_diff:
+            results.append(True)
+        else:
+            results.append(False)
+    return results
+
+with open("C:/Users/Life's Good/Desktop/AdventCode/Day2/input.txt") as f:
+    for line in f:
+        numbers = list(map(int, line.strip().split()))
+        lists.append(numbers)
+
+results = check_asc_or_desc(lists)
+print(results.count(True))
