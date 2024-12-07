a7b345e614ec521e65dc3a701656fe3ef48f50ed
Agon Ajredini <agonajrediniyt@gmail.com>
2024-12-07 21:03:44 +0100

    First commit

---
+++ Day1/Day1.py
@@ -0,0 +1,18 @@
+left_numbers = []
+right_numbers = []
+difference = []
+
+with open("C:/Users/Life's Good/Desktop/AdventCode/Day1/input.txt") as f:
+    for line in f:
+        parts = line.split()
+        
+        left_numbers.append(int(parts[0]))
+        right_numbers.append(int(parts[1]))
+        
+left_numbers.sort()
+right_numbers.sort()
+
+for i in range(len(left_numbers)):
+    difference.append(abs(right_numbers[i] - left_numbers[i]))
+
+print(sum(difference))
---
+++ Day1/Day1pt2.py
@@ -0,0 +1,19 @@
+left_numbers = []
+right_numbers = []
+similarity = []
+
+with open("C:/Users/Life's Good/Desktop/AdventCode/Day1/input.txt") as f:
+    for line in f:
+        parts = line.split()
+        
+        left_numbers.append(int(parts[0]))
+        right_numbers.append(int(parts[1]))
+
+for i in left_numbers:
+    # add = 0
+    # for j in right_numbers:
+    #     if i == j:
+    #         add+=1
+    similarity.append(i*right_numbers.count(i))
+
+print(sum(similarity))
